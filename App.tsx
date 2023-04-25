111254a6d34077e9d787a2c7dda178d4814b3120
ChesterFL <Chester@favorlabs.io>
2023-04-25 14:57:27 +0800

    feat: load fonts

--- App.tsx
+++ App.tsx
@@ -1,28 +1,56 @@
-import React from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import React, {useEffect, useState} from "react";
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Alert} from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import AppNavigator from "./src/navigation";
-import store, { persiStore } from './src/store/index';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react';
+import store, {persiStore} from './src/store/index';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/lib/integration/react';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
 export default function App() {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persiStore}>
-        <SafeAreaProvider initialMetrics={null} style={styles.container}>
-          <SafeAreaView style={{ flex: 1 }}>
-            <StatusBar style="auto" />
-            <AppNavigator />
-          </SafeAreaView>
-        </SafeAreaProvider>
-      </PersistGate>
-    </Provider>
-  );
+    const [fontsLoaded, setFontsLoader] = useState(false);
+    const loadFont = async () => {
+        try {
+            await SplashScreen.preventAutoHideAsync();
+            await Font.loadAsync({
+                'ABeeZee_regular': require('./src/assets/fonts/ABeeZee_regular.ttf'),
+                'Inter_bold': require('./src/assets/fonts/Inter_bold.ttf'),
+                'Inter_light': require('./src/assets/fonts/Inter_light.ttf'),
+                'Inter_medium': require('./src/assets/fonts/Inter_medium.ttf'),
+                'Inter_regular': require('./src/assets/fonts/Inter_regular.ttf'),
+                'Inter_semibold': require('./src/assets/fonts/Inter_semibold.ttf'),
+            });
+        } catch (e) {
+            console.warn(e);
+        } finally {
+            await SplashScreen.hideAsync();
+            setFontsLoader(true);
+        }
+    }
+    useEffect(() => {
+        loadFont().catch(Alert.alert)
+    })
+
+    if (!fontsLoaded) return null;
+
+    return (
+        <Provider store={store}>
+            <PersistGate persistor={persiStore}>
+                <SafeAreaProvider initialMetrics={null} style={styles.container}>
+                    <SafeAreaView style={{flex: 1}}>
+                        <StatusBar style="auto"/>
+                        <AppNavigator/>
+                    </SafeAreaView>
+                </SafeAreaProvider>
+            </PersistGate>
+        </Provider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    //
-  }
+    container: {
+        //
+    }
 });
