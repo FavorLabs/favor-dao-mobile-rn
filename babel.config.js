087d020d7d793a63fb206474a62871f5bc4a4900
ChesterFL <Chester@favorlabs.io>
2023-05-10 16:10:23 +0800

    feat: external nodes can be used for testing

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,11 @@ module.exports = function (api) {
         presets: ['babel-preset-expo'],
         plugins: [
             'react-native-reanimated/plugin',
+            [
+                "module:react-native-dotenv", {
+                "allowUndefined": true,
+            }
+            ]
             // '@babel/plugin-proposal-private-methods',
             // '@babel/plugin-proposal-class-properties'
         ],
